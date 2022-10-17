027544b898ed3ccd6a7b8b09c6a05891a3aadd7e
tanguym77 <tanguy@marmier.me>
2022-10-17 16:46:58 +0200

    update js

---
+++ js/script.js
@@ -0,0 +1,8 @@
+function setActive() {
+    aObj = document.getElementById('nav').getElementsByTagName('a');
+    for(i=0;i<aObj.length;i++) {
+      if(document.location.href.indexOf(aObj[i].href)>=0) {
+        aObj[i].className +=' active' ;
+      }
+    }
+}
