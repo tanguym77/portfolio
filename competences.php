d6e1899fbaa3671f65ef17ae0e958cc054091667
Tanguy <tanguy@marmier.me>
2023-03-11 19:14:55 +0100

    Update all

--- competences.php
+++ competences.php
@@ -9,44 +9,8 @@
             <h1>Mes Compétences</h1>
         </div>
 
-        <!-- CARD -->
-        <div class="row w-75 mx-auto row-cols-1 row-cols-md-2 g-4 p-3">
-            
-            <!-- MOOC ANSSI -->
-            <div class="col">
-                <div class="card">
-                    <div class="card-header">
-                        <h5 class="card-title">MOOC de l'ANSSI</h5>
-                    </div>
-                    <img src="images/certifications/logo_secnum.png" class="card-img-top">
-                    <div class="card-body">
-                        <p class="card-text">Initiation aux principes de la cybersécurité et approfondir ses connaissances pour agir efficacement sur la protection de nos outils numériques.</p>
-                    </div>
-                    <div class="card-footer">
-                        <div class="dropdown text-center">
-                            <a class="btn btn-primary" href="files/certifications/MOOC_ANSSI.pdf" target="_blank">La certification</a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-            <!-- PHP OPENCLASSROOM -->
-            <div class="col">
-                <div class="card">
-                    <div class="card-header">
-                        <h5 class="card-title">PHP & MySQL Openclassroom</h5>
-                    </div>
-                    <img src="images/certifications/logo_php.png" class="card-img-top">
-                    <div class="card-body">
-                        <p class="card-text">Se former aux essentiels de PHP et MySQL.<br><br><br></p>
-                    </div>
-                    <div class="card-footer">
-                        <div class="dropdown text-center">
-                            <a class="btn btn-primary" href="files/certifications/Certification_PHP.PNG" target="_blank">La certification</a>
-                        </div>
-                    </div>
-                </div>
-            </div>
+        <!-- CARDS -->
+        <div class="row mx-auto row-cols-1 row-cols-sm-2 row-cols-md-3 g-4 p-3">
 
             <!-- API PYTHON CISCO-->
             <div class="col">
@@ -54,7 +18,7 @@
                     <div class="card-header">
                         <h5 class="card-title">API Pyton - Cisco</h5>
                     </div>
-                    <img src="images/certifications/logo_api.png" class="card-img-top">
+                    <img src="images/certifications/logo_python.webp" class="card-img-top">
                     <div class="card-body">
                         <p class="card-text">Formation proposé par CISCO qui permet de se former aux API avec WEBEX, Postman et Python.</p>
                     </div>
@@ -65,7 +29,61 @@
                     </div>
                 </div>
             </div>
+
+            <!-- JAVASCRIPT -->
+            <div class="col">
+                <div class="card">
+                    <div class="card-header">
+                        <h5 class="card-title">Javascript - Essentials</h5>
+                    </div>
+                    <img src="images/certifications/logo_javascript.png" class="card-img-top">
+                    <div class="card-body">
+                        <p class="card-text">Une formation CISCO qui m'a permis d'apprendre les basics de javascript pour créer des algorithmes simple.</p>
+                    </div>
+                    <div class="card-footer">
+                        <div class="dropdown text-center">
+                            <a class="btn btn-primary" href="files/certifications/TANGUYMARMIER-Javascript-certificate.pdf" target="_blank">La certification</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <!-- MOOC ANSSI -->
+            <div class="col">
+                <div class="card">
+                    <div class="card-header">
+                        <h5 class="card-title">MOOC de l'ANSSI</h5>
+                    </div>
+                    <img src="images/certifications/cyber.jpg" class="card-img-top">
+                    <div class="card-body">
+                        <p class="card-text">Initiation aux principes de la cybersécurité et approfondir ses connaissances pour agir efficacement sur la protection de nos outils numériques.</p>
+                    </div>
+                    <div class="card-footer">
+                        <div class="dropdown text-center">
+                            <a class="btn btn-primary" href="files/certifications/MOOC_ANSSI.pdf" target="_blank">La certification</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
             
+            <!-- PHP OPENCLASSROOM -->
+            <div class="col">
+                <div class="card">
+                    <div class="card-header">
+                        <h5 class="card-title">PHP & MySQL Openclassroom</h5>
+                    </div>
+                    <img src="images/certifications/logo_php.png" class="card-img-top">
+                    <div class="card-body">
+                        <p class="card-text">Une formation Openclassroom pour confirmer mon niveau en PHP et MySQL.<br><br><br></p>
+                    </div>
+                    <div class="card-footer">
+                        <div class="dropdown text-center">
+                            <a class="btn btn-primary" href="files/certifications/Certification_PHP.PNG" target="_blank">La certification</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
         </div>
     </div>
 </section>
